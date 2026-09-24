be71a51c32f77875cd17e5decb7f09c71a96e399
joseferrar <joseferrar4@gmail.com>
2026-09-24 14:46:30 +0530

    si

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {retryWrites: false, directConnection: true});
+        await mongoose.connect(process.env.MONGO_URI, {retryWrites: false});
 
         console.log("MongoDB connected");
     } catch (error) {
